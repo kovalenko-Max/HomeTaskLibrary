e43ae3d6a106de84b94e61ab79dd79c8856b2665
Maks Kovalenko <max.don@live.ru>
2021-03-07 17:03:08 +0200

    add in Cycles

--- HomeTaskLibrary/ConsoleApp.cs
+++ HomeTaskLibrary/ConsoleApp.cs
@@ -8,21 +8,9 @@ namespace HomeTaskLibrary
     {
         public static void Main(string[] args)
         {
-            int[,] array = TwoDimensionalArrays.GetRandomArray(6, 3, 100);
-            TwoDimensionalArrays.Print(array);
+            int numb = 64;
 
-            int minVal = TwoDimensionalArrays.GetMinElement(array);
-            Tuple<int, int> indexOfMinValue = TwoDimensionalArrays.GetIndexOfMinElement(array);
-            int maxVal = TwoDimensionalArrays.GetMaxElement(array);
-            Tuple<int, int> indexOfMaxValue = TwoDimensionalArrays.GetIndexMaxElement(array);   
-                        
-            Console.WriteLine("Max Value = " + maxVal + " " + indexOfMaxValue + "\nMin Value = " + minVal + " " + indexOfMinValue);
-
-            Console.WriteLine("Count = " + TwoDimensionalArrays.CountElementsBiggerThanNeighbors(array));
-
-            int[,] tArray = TwoDimensionalArrays.GetTranspositionArray(array);
-
-            TwoDimensionalArrays.Print(tArray);
+            Console.WriteLine(Cycles.GetNumbersWhichSumOfEvenIsBiggerThanOdd(numb));
 
         }
     }
